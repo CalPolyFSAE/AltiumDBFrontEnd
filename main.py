7069545fe08d6df5fdda44db50f33688b741854b
Nick Mah <nickjmah@gmail.com>
2018-08-15 19:16:23 -0700

    Got it to work

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import json
 import urllib.request
+import urllib.parse
 
 
 def main():
     url = 'https://octopart.com/api/v3/parts/search?'
-    url += '&queries=[{"mpn":"SN74S74N","offers.seller.name":"Digi-Key"}]'
+    args = [
+        ('q', 'SN74S74N'),
+        ('offers.seller.name', 'Digi-Key'),
+        ('sortby', 'part["offers"][0]["prices"]["USD"][1][1]')
+    ]
+    url += '&' + urllib.parse.urlencode(args)
     url += '&apikey=2a67f89b'
 
+    print(url)
     data = urllib.request.urlopen(url).read()
     response = json.loads(data)
+    print(response['hits'])
+
     for result in response['results']:
-        for item in result['items']:
-            print(item['offers'])
+        part = result['item']
+        print(part['offers'][0]['prices']['USD'][1][1])
+        print(part['brand']['name'],part['mpn'])
 
 
 
