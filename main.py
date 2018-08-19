b85368d82310e5cec477a15574bb66ff14a3bbc6
Nick Mah <nickjmah@gmail.com>
2018-08-18 21:29:58 -0700

    Started partmaker class

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import json
 import urllib.request
 import urllib.parse
-
+import octopart
 
 def main():
-    url = 'https://octopart.com/api/v3/parts/search?'
-    args = [
-        ('q', 'SN74S74N'),
-        ('offers.seller.name', 'Digi-Key'),
-        ('sortby', 'part["offers"][0]["prices"]["USD"][1][1]')
-    ]
-    url += '&' + urllib.parse.urlencode(args)
-    url += '&apikey=2a67f89b'
+    results = octopart.match(['ERJ-2GE0R00X'])
+    print(results)
+    results = octopart.search(['NA'])
+    # url = 'https://octopart.com/api/v3/parts/search?'
+    # args = [
+    #     (
+    #     ('offers.seller.name', 'Digi-Key'),
+    #     ('sortby', 'part["offers"][0]["prices"]["USD"][1][1]')
+    # ]
+    # url += '&' + urllib.parse.urlencode(args)
+    # url += '&apikey=2a67f89b'
 
-    print(url)
-    data = urllib.request.urlopen(url).read()
-    response = json.loads(data)
-    print(response['hits'])
+    # print(url)
+    # data = urllib.request.urlopen(url).read()
+    # response = json.loads(data)
+    # print(response['hits'])
 
-    for result in response['results']:
-        part = result['item']
-        print(part['offers'][0]['prices']['USD'][1][1])
-        print(part['brand']['name'],part['mpn'])
+    # for result in response['results']:
+    #     part = result['item']
+    #     print(part['offers'][0]['prices']['USD'][1][1])
+    #     print(part['brand']['name'],part['mpn'])
 
 
 
---
+++ partMaker.py
@@ -0,0 +1,5 @@
+# An Assistant class to help create a bunch of parts to be added
+class partMaker:
+    def __init__(self, names, ):
+        self.names = names
+        
