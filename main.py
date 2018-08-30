97ffc0091e5ac159be7cb975ce18787d4ca13105
Nick Mah <nickjmah@gmail.com>
2018-08-30 12:38:30 -0700

    Updated Resistors to include 10% tolerances

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import csv
 def main():
     # Creating main connection
     # Resistors
-    tolerances = [1, 5]
-    E12 = [1, 1.2, 1.5,
+    tolerances = [1, 5, 10]
+    E12 = [1.0, 1.2, 1.5,
            1.8, 2.2, 2.7,
            3.3, 3.9, 4.7,
            5.6, 6.8, 8.2]
--- part.py
+++ part.py
@@ -1,9 +1,26 @@
 class Part:
-    def __init__(self, manufacturer, drawer, name, value, package, mfrPartNo,
+    def __init__(self, manuID, drawer, name, value, package, mfrPartNo,
                  purchaseLink, description, comment, datasheetLink, supplier, supplierPN,
                  creator, libraryRef, libraryPath, footprintRef, footprintPath):
-        self.name = name
-        self.value = value
-        self.manufacturer = manufacturer
-        self.drawer = drawer
-        self.id = id
+
+        self.name = None
+        self.manID = None
+        self.drawerID = None
+        self.value = None
+        self.package = None
+        self.manPN = None
+        self.PNLink = None
+        self.desc = None
+        self.comment = None
+        self.datasheet = None
+        self.supplier = 'Digi-Key'
+        self.suppPN = None
+        self.creator = None
+        self.libRef = None
+        self.libPath = None
+        self.footRef = None
+        self.footPath = None
+        self.genName()
+
+    def genName(self):
+        pass
